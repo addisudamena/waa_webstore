528ff2762c12a824852d6d52dc6dc080cff71dc8
Addisu Damena Alemu <addisudamena49@gmail.com>
2021-08-09 18:09:49 -0500

    Update ProductQuery.java

--- BackEnd/src/main/java/waa/alemu/addisu/webstor/web/ProductQuery.java
+++ BackEnd/src/main/java/waa/alemu/addisu/webstor/web/ProductQuery.java
@@ -1,10 +1,10 @@
 package waa.alemu.addisu.webstor.web;
-
+import org.springframework.stereotype.Component;
 import waa.alemu.addisu.webstor.service.ProductDTO;
 import waa.alemu.addisu.webstor.service.ProductService;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -16,11 +16,14 @@ public class ProductQuery implements GraphQLQueryResolver {
     @Autowired
     private ProductService productService;
 
+    
+     public Optional<ProductDTO> getProduct(final int productNumber) {
+        return Optional.of(productService.findByProductNumber(productNumber));
+    }
+    
     public List<ProductDTO> getProducts() {
         return productService.findAll().stream().collect(Collectors.toList());
     }
 
-    public Optional<ProductDTO> getProduct(final int productNumber) {
-        return Optional.of(productService.findByProductNumber(productNumber));
-    }
+   
 }
